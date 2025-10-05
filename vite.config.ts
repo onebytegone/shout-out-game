6c6b3c3a5dad3bfc211308a16415b60ee014b060
Ethan Smith <ethan@onebytegone.com>
2025-10-05 18:31:22 -0400

    fix: resolve deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
    plugins: [ svelte() ],
-   base: process.env.NODE_ENV === 'production' ? '/shout-out/' : '/',
+   base: process.env.NODE_ENV === 'production' ? '/shout-out-game/' : '/',
    build: {
       outDir: 'dist',
       sourcemap: true,
